0e24f03de2c33886441ec5b6739f766995e4dd96
root-344 <michimasa8@gmail.com>
2022-01-10 15:45:59 +0900

    devise導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(_resource)
+    root_path
+  end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+  end
+  
+  # def set_side
+  #   @users = user.all
+  # end
 end
