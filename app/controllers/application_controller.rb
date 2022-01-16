7984a3a21c1353a31be42215530614f70464c96d
root-344 <michimasa8@gmail.com>
2022-01-16 16:04:52 +0900

    before_action追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :set_side
+  
   def after_sign_in_path_for(_resource)
     root_path
   end
@@ -12,7 +13,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
   end
 
-  # def set_side
-  #   @users = user.all
-  # end
+  def set_side
+    @users = User.all
+  end
 end
