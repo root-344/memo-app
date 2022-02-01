fd566730d0af5b0124afb8242640aa9fb4db2686
root-344 <michimasa8@gmail.com>
2022-02-01 17:25:55 +0900

    アソシエーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_users, through: :likes, source: :user
+  has_one_attached :image
 
   def self.search(search)
     if search != ''
